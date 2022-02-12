fad6192b3f413231f5401b22bd0b0871e9543eb3
malleswar-reddy <56329087+malleswar-reddy@users.noreply.github.com>
2022-02-12 09:25:36 +0530

    Hello

--- src/main/java/com/main/TestMain.java
+++ src/main/java/com/main/TestMain.java
@@ -2,6 +2,6 @@ package com.main;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("Hellow World");
+        System.out.println("Hello World");
     }
 }
